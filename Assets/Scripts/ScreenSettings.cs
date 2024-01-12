13a2c3875eb686600123ac118ad5f6169313780e
tim <guntertimofej4@gmail.com>
2024-01-12 11:59:20 +0600

    Some changes with added menu

---
+++ Assets/Scripts/ScreenSettings.cs
@@ -0,0 +1,39 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEngine.SceneManagement;
+
+// public class ScreenSettings : MonoBehaviour
+// {
+//     public GameObject pausePanel;
+//     // public int level;
+
+//     public void PauseButtonPressed()
+//     {
+//         pausePanel.SetActive(true);
+//         Time.timeScale = 0f;
+
+//     }
+
+//     public void ContinueButtonPressed()
+//     {
+//         pausePanel.SetActive(false);
+//         Time.timeScale = 1f;
+
+//     }
+
+//     public void RestartButtonPressed()
+//     {
+//         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+//         Time.timeScale = 1f;
+
+//     }
+
+//     public void BackToMenu()
+//     {
+//         SceneManager.LoadScene(0);
+//         Time.timeScale = 1f;
+
+//     }
+
+// }
